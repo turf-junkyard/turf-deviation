627f0d47cd2e16e74b00bfe78c2686cedec5be1f
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-03 16:16:23 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -11,43 +11,102 @@ var inside = require('turf-inside');
 * @param {String} outField the field to append to `polygons` representing deviation
 * @return {FeatureCollection} a FeatureCollection of Polygon features with appended field representing deviation
 * @example
-* var polygons = turf.featurecollection([
-*   turf.polygon([[
-*     [-97.807159, 30.270335],
-*     [-97.807159, 30.369913],
-*     [-97.612838, 30.369913],
-*     [-97.612838, 30.270335],
-*     [-97.807159, 30.270335]
-*   ]]),
-*   turf.polygon([[
-*     [-97.825698, 30.175405],
-*     [-97.825698, 30.264404],
-*     [-97.630691, 30.264404],
-*     [-97.630691, 30.175405],
-*     [-97.825698, 30.175405]
-*   ]])
-* ]);
-* var points = turf.featurecollection([
-*   turf.point([-97.709655, 30.311245],
-*     {population: 500}),
-*   turf.point([-97.766647, 30.345028],
-*     {population: 400}),
-*   turf.point([-97.765274, 30.294646],
-*     {population: 600}),
-*   turf.point([-97.753601, 30.216355],
-*     {population: 500}),
-*   turf.point([-97.667083, 30.208047],
-*   {population: 200})
-* ]);
+* var polygons = {
+*   "type": "FeatureCollection",
+*   "features": [
+*     {
+*       "type": "Feature",
+*       "properties": {},
+*       "geometry": {
+*         "type": "Polygon",
+*         "coordinates": [[
+*           [-97.807159, 30.270335],
+*           [-97.807159, 30.369913],
+*           [-97.612838, 30.369913],
+*           [-97.612838, 30.270335],
+*           [-97.807159, 30.270335]
+*         ]]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {},
+*       "geometry": {
+*         "type": "Polygon",
+*         "coordinates": [[
+*           [-97.825698, 30.175405],
+*           [-97.825698, 30.264404],
+*           [-97.630691, 30.264404],
+*           [-97.630691, 30.175405],
+*           [-97.825698, 30.175405]
+*         ]]
+*       }
+*     }
+*   ]
+* };
+* var points = {
+*   "type": "FeatureCollection",
+*   "features": [
+*     {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 500
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [-97.709655, 30.311245]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 400
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [-97.766647, 30.345028]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 600
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [-97.765274, 30.294646]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 500
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [-97.753601, 30.216355]
+*       }
+*     }, {
+*       "type": "Feature",
+*       "properties": {
+*         "population": 200
+*       },
+*       "geometry": {
+*         "type": "Point",
+*         "coordinates": [-97.667083, 30.208047]
+*       }
+*     }
+*   ]
+* };
 *
-* var inField = 'population';
-* var outField = 'pop_deviation';
+* var inField = "population";
+* var outField = "pop_deviation";
 *
 * var deviated = turf.deviation(
 *   polygons, points, inField, outField);
 *
-* var result = turf.featurecollection(
-*   points.features.concat(deviated.features));
+* var resultFeatures = points.features.concat(
+*   deviated.features);
+* var result = {
+*   "type": "FeatureCollection",
+*   "features": resultFeatures
+* };
 *
 * //=result
 */
